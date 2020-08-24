d6581e42cf84c58eca5fad1b3090a437be749032
Martin Aviles <mgmbamartin@gmail.com>
2020-08-24 17:08:59 +0200

    created database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :listings
+  has_many :offers
+  has_many :received_offers, source: :offers , through: :listings, class_name: "Offer"
+  has_many :pending_listings, source: :listing ,through: :offers, class_name: "Listing", foreign_key: :listing_id
 end
--- db/migrate/20200824132631_create_offers.rb
+++ db/migrate/20200824132631_create_offers.rb
@@ -2,10 +2,10 @@ class CreateOffers < ActiveRecord::Migration[6.0]
   def change
     create_table :offers do |t|
       t.string :description
-      t.integer :listingid
-      t.integer :userid
-      t.string :startdate
-      t.string :enddate
+      t.references :listing, foreign_key: true
+      t.references :user, foreign_key: true
+      t.string :start_date
+      t.string :end_date
       t.boolean :status
 
       t.timestamps
