74c56c4519f0eac893d28ec7cea59c42fdd59fc0
Chris Kirk <uberengineer@gmail.com>
2020-08-25 17:16:04 +0200

    Fixed registration controller array

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,12 +42,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute], :first_name, :last_name, :username, :gender, :age)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :first_name, :last_name, :username, :gender, :age])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute], :first_name, :last_name, :username, :gender, :age)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :first_name, :last_name, :username, :gender, :age])
   end
 
   # The path used after sign up.
