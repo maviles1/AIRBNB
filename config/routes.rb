a52fa3d620ce660ec53e06962bd72cdd7ba8166b
Martin Aviles <mgmbamartin@gmail.com>
2020-08-25 14:11:37 +0200

    midway commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '', to: "pages#home"
   get 'listings', to: "listings#index"
-  get 'listings/new', to: "listings#new"
+  get 'listings/new', to: "listings#new", as: "new_listing"
   post 'listings', to: "listings#create"
   get 'listings/:id', to: "listings#show"
   get 'listings/:id/edit', to: "listings#edit"
