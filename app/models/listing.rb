d76dd23be3a23f54b15474e99f756a0e537afee7
Martin Aviles <mgmbamartin@gmail.com>
2020-08-27 18:15:55 +0200

    add migrations + charles magic

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,6 +4,5 @@ class Listing < ApplicationRecord
   has_many_attached :img_urls
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-
-
+  scope :available_for, -> (start_date, end_date) { joins(:offers).where("offers.start_date: >= ? OR end_date: <= ?", start_date, end_date)}
 end
