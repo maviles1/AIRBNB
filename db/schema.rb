e9287d48f232a36da12beeca82991adf0cc33d15
Martin Aviles <mgmbamartin@gmail.com>
2020-08-27 14:27:06 +0200

    edit_db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_141437) do
+ActiveRecord::Schema.define(version: 2020_08_27_121714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,11 +50,11 @@ ActiveRecord::Schema.define(version: 2020_08_26_141437) do
   end
 
   create_table "offers", force: :cascade do |t|
-    t.string "description"
+    t.text "description"
     t.bigint "listing_id"
     t.bigint "user_id"
-    t.string "start_date"
-    t.string "end_date"
+    t.date "start_date"
+    t.date "end_date"
     t.boolean "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
